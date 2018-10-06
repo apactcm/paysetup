6a68c536c3abd6f5f2d98a84366de741ded7161d
apactcm <43777318+apactcm@users.noreply.github.com>
2018-10-06 08:56:53 +0800

    Update setup.js

--- server/node/setup.js
+++ server/node/setup.js
@@ -24,21 +24,52 @@ module.exports = {
         // Create a few products and SKUs assuming they don't already exist.
         try {
           // Increment Magazine.
-          const membership = await stripe.products.create({
+          const increment = await stripe.products.create({
             id: 'increment',
             type: 'good',
-            name: '会员服务',
+            name: 'Increment Magazine',
             attributes: ['issue'],
           });
           await stripe.skus.create({
-            id: 'increment-01',
-            product: 'VIP会员',
-            attributes: {valid: '年费'},
-            price: 100,
+            id: 'increment-03',
+            product: 'increment',
+            attributes: {issue: 'Issue #3 “Development”'},
+            price: 399,
             currency: config.currency,
             inventory: {type: 'infinite'},
           });
-          
+
+          // Stripe Shirt.
+          const shirt = await stripe.products.create({
+            id: 'shirt',
+            type: 'good',
+            name: 'Stripe Shirt',
+            attributes: ['size', 'gender'],
+          });
+          await stripe.skus.create({
+            id: 'shirt-small-woman',
+            product: 'shirt',
+            attributes: {size: 'Small Standard', gender: 'Woman'},
+            price: 999,
+            currency: config.currency,
+            inventory: {type: 'infinite'},
+          });
+
+          // Stripe Pins.
+          const pins = await stripe.products.create({
+            id: 'pins',
+            type: 'good',
+            name: 'Stripe Pins',
+            attributes: ['set'],
+          });
+          await stripe.skus.create({
+            id: 'pins-collector',
+            product: 'pins',
+            attributes: {set: 'Collector Set'},
+            price: 799,
+            currency: config.currency,
+            inventory: {type: 'finite', quantity: 500},
+          });
           console.log('Setup complete.');
           resolve();
           this.running = false;
